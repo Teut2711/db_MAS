d4e01191201e079ca71bd0b262a3fb7f946f3f06
Vishesh Mangla <manglavishesh64@gmail.com>
2020-07-01 21:49:12 +0530

    Code Ready to Feed Data

--- database/backend/nsdl/interface.py
+++ database/backend/nsdl/interface.py
@@ -53,28 +53,30 @@ class Interface(ABC):
 
     @staticmethod
     def get_cols(*args):
-        
+
         with open(os.path.join(*args), "rb") as p:
             cols = pickle.load(p)
         return cols
+
+    @staticmethod
+    def get_model_object(model, all_cols, model_cols, chunk_piece):
+        chunk_piece = [i.strip() for i in chunk_piece]
+        chunk_piece = dict(zip(all_cols, chunk_piece))
+        chunk_piece = model(**{key: val
+                             for key, val in chunk_piece.items()
+                             if key in model_cols})
+        return chunk_piece
 
     def create_database(self):
         for chunk in self.get_data(self.file_obj):
 
             self.inserts += len(chunk)
+
             self.model.objects.bulk_create(
+                [self.get_model_object(self.model, self.all_cols, self.model_cols, i)
+                    for i in chunk], ignore_conflicts=True)
 
-                [
-                    {
-                        key: val
-                        for key, val in dict(
-                            zip(self.all_cols, i)
-                        ).items() if key in self.model_cols
-                    }
-                    for i in chunk
-                ], ignore_conflicts=True)
-
-    @staticmethod
+    @ staticmethod
     def get_data(file_obj):
         # return iterator (type annotations are conflicting with the comments in vscode)
 
@@ -82,13 +84,13 @@ class Interface(ABC):
 
         def chunk_to_dict_values(chunk):
             # get value at index 1 and 2
-            isin, date = chunk[0].split("##")[1:3]
+            isin, date = chunk[0].split("##")[1: 3]
 
             chunk = chunk[1:]
 
             return [(i.split("##")[2:] + [isin, date])
                     for i in chunk]   # this is a list of lists
-            
+
         # from nodeirc by @_habnabit
         for chunk in split_before(
             (l.decode().strip() for l in file_obj),
@@ -98,6 +100,6 @@ class Interface(ABC):
             yield chunk_to_dict_values(chunk)
         # from nodeirc by @_habnabit
 
-    @abstractmethod
+    @ abstractmethod
     def update_database(self):
         pass
--- database/backend/run.py
+++ database/backend/run.py
@@ -1,14 +1,19 @@
 from .nsdl import dematad
 from database import models
+from configparser import RawConfigParser
+
+config = RawConfigParser()
+config.read('config.cfg')
 
 
 def main(file_obj):
-    if not models.Dematad.objects.exists():
+    if config["RUN"]["TASK"] == "CREATE":
         stats_Dematad = dematad.Dematad.clear_and_fill(
             models.Dematad, file_obj)
-    else:
+    elif config["RUN"]["TASK"] == "UPDATE":
         stats_Dematad = dematad.update_or_create(
             models.Dematad, file_obj)
+        
     return {
         "uploadedFileRows": stats_Dematad.uploaded_file_rows,
         "finalRows": stats_Dematad.final_rows,
